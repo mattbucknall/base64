3607015a8d6aa83d538c4a2330d26d07c563aa13
Matthew Bucknall <matthew.bucknall@gmail.com>
2019-12-04 05:26:01 +0000

    Added missing return statement to encoder function

--- base64.c
+++ base64.c
@@ -99,7 +99,8 @@ base64_result_t base64_from_raw(void* base64_buffer, size_t base64_size,
     int with_lines = options ? options->with_lines : 0;
     int null_terminate = options ? options->null_terminate : 0;
 
-    if ( base64_size < base64_buffer_size_from_raw_buffer_size(raw_size, options) ) return BASE64_RESULT_BUFFER_TOO_SMALL;
+    if ( base64_size < base64_buffer_size_from_raw_buffer_size(raw_size, options) )
+            return BASE64_RESULT_BUFFER_TOO_SMALL;
 
     in_i = (const unsigned char*) raw_buffer;
     in_e = in_i + raw_size;
@@ -148,4 +149,6 @@ base64_result_t base64_from_raw(void* base64_buffer, size_t base64_size,
 
     if ( with_lines && line_length > 0 ) *out_i++ = '\n';
     if ( null_terminate ) *out_i = '\0';
+
+    return BASE64_RESULT_OK;
 }
